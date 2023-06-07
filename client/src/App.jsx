ec887c6b97695b46b27c4b7da09c7be685258b35
Edward <edward.nayve@boom.ai>
2023-06-07 20:03:57 +0800

    Adjust main container margin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -64,7 +64,7 @@ const App = () => {
             </ul>
           </nav>
         </header>
-        <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] h-screen'>
+        <main className='sm:p-8 px-4 w-full bg-[#f9fafe] h-screen'>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/create-post' element={<CreatePost />} />
